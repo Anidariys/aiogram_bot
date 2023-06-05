fce7931660ce71df9c35d7c4440c2757a7e83297
Amadeus <anidariys@gmail.com>
2023-06-05 19:32:10 +0300

    add ngrok path

--- main.py
+++ main.py
@@ -5,7 +5,8 @@ from bot import dp, bot, config
 
 app = FastAPI()
 WEBHOOK_PATH = f"/bot/{config.tg_bot.token}"
-WEBHOOK_URL = "https://9c19-46-172-156-66.eu.ngrok.io" + WEBHOOK_PATH
+NGROK_URL = "https://9c19-46-172-156-66.eu.ngrok.io"
+WEBHOOK_URL = NGROK_URL + WEBHOOK_PATH
 
 
 @app.on_event("startup")
